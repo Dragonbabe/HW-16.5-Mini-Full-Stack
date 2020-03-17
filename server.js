1e42447f8919ebfbde724dd2a3c56d37851da296
Esther Walker <equestriennesrule@yahoo.com>
2020-03-17 16:40:30 -0700

    trying to figure out why npm init is wierd

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.get('/myroute', (req, res)=>{
+    res.send('hey you hit my route!');
+});
+
 app.listen(PORT, () => console.log('Hello world!'));
